a9f85305c39ed0f7469875bcb66b1c6018667daf
Derek Rosa Cavalcanti Bezerra <derek.cavalcanti1@gmail.com>
2025-09-19 16:16:04 -0300

    moving botdemissao to root folder

--- BotDemissao/config.py
+++ BotDemissao/config.py
@@ -1,6 +1,4 @@
 import os; token = os.environ.get('token_bot_demissao')
 
-TOKEN = "8405163283:AAHnsRop-Dq_57Al2yUeGsU0yXoCbZyv2vk"
-
 CREDS_FILE = "bot-demissao-telegram-289972e0f194.json" 
 SHEET_NAME = "Forms Demissão (respostas)"
